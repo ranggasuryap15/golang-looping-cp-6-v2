4ccc89b49e2afe5d29b1df7ca8eeaf2f1542b989
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-31 22:36:38 +0700

    selesai

--- main.go
+++ main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func BiggestPairNumber(numbers int) int {
-	return 0 // TODO: replace this
+	numStr := strconv.Itoa(numbers)
+
+	var pair = 0
+	var biggest = 0
+
+	
+	for i := 1; i < len(numStr); i++ {
+		num1, _ := strconv.Atoi(string(numStr[i]))
+		num2, _ := strconv.Atoi(string(numStr[i-1]))
+
+		jumlah := num1 + num2
+		if jumlah > biggest {
+			biggest = jumlah
+
+			// gabungkan angkanya
+			pairNum, _ := strconv.Atoi(string(numStr[i-1]) + string(numStr[i]))
+			pair = pairNum
+		}
+	}
+
+	return pair // TODO: replace this
 }
 
 // gunakan untuk melakukan debug
